b9d849c01bd2534e7fc04b8eccd9df7934b53167
Solomon Tang <solomoncztang@gmail.com>
2020-07-14 23:03:50 -0700

    Add ol portrait

--- src/assets/svg/icons/index.js
+++ src/assets/svg/icons/index.js
@@ -5,12 +5,19 @@
 import styled from 'styled-components';
 
 import OpenLatticeIconSVG from './ol-icon.svg';
+import olPortraitSVG from './ol-portrait.svg';
 
 const OpenLatticeIcon = styled.img.attrs({
   alt: 'openlattice-icon',
   src: OpenLatticeIconSVG,
 })``;
 
+const OLPortrait = styled.img.attrs({
+  alt: 'ol-portrait',
+  src: olPortraitSVG,
+})``;
+
 export {
+  OLPortrait,
   OpenLatticeIcon,
 };
