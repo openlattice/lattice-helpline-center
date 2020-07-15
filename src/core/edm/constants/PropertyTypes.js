08bcf2528b81d9ac98572f8868e2272027f6f58d
Solomon Tang <solomoncztang@gmail.com>
2020-07-14 23:04:24 -0700

    add person property types

--- src/core/edm/constants/PropertyTypes.js
+++ src/core/edm/constants/PropertyTypes.js
@@ -9,6 +9,9 @@ const { FQN } = Models;
 const PROPERTY_TYPE_FQNS = {
   OL_DATE_TIME: FQN.of('ol.datetime'),
   OL_ID: FQN.of('ol.id'),
+  GIVEN_NAME: FQN.of('nc.PersonGivenName'),
+  SURNAME: FQN.of('nc.PersonSurName'),
+  DOB: FQN.of('nc.PersonBirthDate'),
 };
 
 export default PROPERTY_TYPE_FQNS;
