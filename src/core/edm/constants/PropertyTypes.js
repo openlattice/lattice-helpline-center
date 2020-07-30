494acbb0fd6c8cd49b6c275f6346618dbdaa1d3f
Solomon Tang <solomoncztang@gmail.com>
2020-07-30 12:14:00 -0700

    add more properties

--- src/core/edm/constants/PropertyTypes.js
+++ src/core/edm/constants/PropertyTypes.js
@@ -7,12 +7,15 @@ import { Models } from 'lattice';
 const { FQN } = Models;
 
 const PROPERTY_TYPE_FQNS = {
+  CODE: FQN.of('ol.code'),
   DATE_TIME: FQN.of('ol.datetime'),
   DOB: FQN.of('nc.PersonBirthDate'),
   GIVEN_NAME: FQN.of('nc.PersonGivenName'),
   ID: FQN.of('ol.id'),
   NAME: FQN.of('ol.name'),
+  SCORE_VALUE: FQN.of('ol.scorevalue'),
   SURNAME: FQN.of('nc.PersonSurName'),
+  TITLE: FQN.of('ol.title'),
   VALUES: FQN.of('ol.values'),
 };
 
