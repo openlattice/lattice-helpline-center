19aca94530013f9c8616be5a821612f11fab5d2d
Solomon Tang <solomoncztang@gmail.com>
2020-07-30 12:46:36 -0700

    expose initalizeHelplineStore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import initializeHelplineStore from './core/redux/ReduxStore';
+
 export {
   GreatestNeeds,
   ProfileCard,
   ProfileContainer,
   SelfSufficiencyMatrix,
   SurveyHistory,
+  initializeHelplineStore,
 } from './components/profile';
