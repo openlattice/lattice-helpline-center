ba558f2ffbf04a97d6568131c48e9cfb769b786a
Solomon Tang <solomoncztang@gmail.com>
2020-08-03 21:27:33 -0700

    add breadcrumbs

---
+++ src/components/breadcrumbs/index.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components';
+import { Colors } from 'lattice-ui-kit';
+import { Link } from 'react-router-dom';
+
+const { NEUTRAL, PURPLE } = Colors;
+
+const breadcrumbStyle = css`
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+const BreadcrumbLink = styled(Link)`
+  ${breadcrumbStyle};
+  color: ${PURPLE.P300};
+  text-decoration: none;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const BreadcrumbItem = styled.span`
+  ${breadcrumbStyle};
+  color: ${NEUTRAL.N500};
+`;
+
+export {
+  BreadcrumbItem,
+  BreadcrumbLink,
+};
