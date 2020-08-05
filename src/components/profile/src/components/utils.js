f47c892c1ec94a60fe361e4fdcf202904bb7e7e8
Solomon Tang <solomoncztang@gmail.com>
2020-08-04 19:03:26 -0700

    format survey data

--- src/components/profile/src/components/utils.js
+++ src/components/profile/src/components/utils.js
@@ -1,7 +1,14 @@
 // @flow
-import { Map, getIn } from 'immutable';
+import {
+  Map,
+  OrderedMap,
+  fromJS,
+  getIn,
+} from 'immutable';
 
 import { PropertyTypes } from '../../../../core/edm/constants';
+import { getPropertyValue } from '../../../../utils/EntityUtils';
+import { CATEGORY_BY_QUESTION_NUMBER } from '../sagas/constants';
 
 const getFirstLastFromPerson = (person :Map | Object) => {
   const firstName = getIn(person, [PropertyTypes.GIVEN_NAME, 0], '').trim();
@@ -10,6 +17,26 @@ const getFirstLastFromPerson = (person :Map | Object) => {
   return `${firstName} ${last}`;
 };
 
+const formatSurveyData = (questions :Map, answers :Map) :OrderedMap => {
+  const surveyData = fromJS(CATEGORY_BY_QUESTION_NUMBER)
+    .sortBy((category, number) => parseInt(number, 10))
+    .reduce((grouped, category, number) => {
+      const entry = questions.findEntry((q) => getPropertyValue(q, PropertyTypes.CODE) === number);
+      if (entry) {
+        const [answerId, question] = entry;
+        const answer = answers.get(answerId);
+        if (grouped.has(category)) {
+          return grouped.mergeIn([category], fromJS([{ answer, question }]));
+        }
+        return grouped.set(category, fromJS([{ answer, question }]));
+      }
+      return grouped;
+    }, OrderedMap());
+
+  return surveyData;
+};
+
 export {
-  getFirstLastFromPerson
+  formatSurveyData,
+  getFirstLastFromPerson,
 };
