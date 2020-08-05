7fa485988fe6bcb04aa29b7d5ba6097dd0506282
Solomon Tang <solomoncztang@gmail.com>
2020-08-04 19:05:42 -0700

    add breadcrumbs to profile container

--- src/components/profile/src/components/ProfileContainer.js
+++ src/components/profile/src/components/ProfileContainer.js
@@ -2,10 +2,15 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { StyleUtils } from 'lattice-ui-kit';
+import { Breadcrumbs, StyleUtils } from 'lattice-ui-kit';
 
 import ProfileAside from './ProfileAside';
 import ProfileBody from './ProfileBody';
+import { useSelector } from './HelplineProvider';
+import { getFirstLastFromPerson } from './utils';
+
+import { BreadcrumbItem, BreadcrumbLink, BreadcrumbWrapper } from '../../../breadcrumbs';
+import { PROFILE_PATHS } from '../sagas/constants';
 
 const { media } = StyleUtils;
 
@@ -22,11 +27,24 @@ type Props = {
   personId :UUID;
 };
 
-const ProfileContainer = ({ personId } :Props) => (
-  <ProfileGrid>
-    <ProfileAside />
-    <ProfileBody personId={personId} />
-  </ProfileGrid>
-);
+const ProfileContainer = ({ personId } :Props) => {
+  const person = useSelector((store) => store.getIn(PROFILE_PATHS.person));
+  const name = getFirstLastFromPerson(person);
+
+  return (
+    <div>
+      <BreadcrumbWrapper>
+        <Breadcrumbs>
+          <BreadcrumbLink to="/">Home</BreadcrumbLink>
+          <BreadcrumbItem>{name}</BreadcrumbItem>
+        </Breadcrumbs>
+      </BreadcrumbWrapper>
+      <ProfileGrid>
+        <ProfileAside />
+        <ProfileBody personId={personId} />
+      </ProfileGrid>
+    </div>
+  );
+};
 
 export default ProfileContainer;
