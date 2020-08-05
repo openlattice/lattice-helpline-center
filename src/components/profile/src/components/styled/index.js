5bb6cdc5789b2facdbe569946f224b1ce203979e
Solomon Tang <solomoncztang@gmail.com>
2020-08-04 22:43:50 -0700

    bring back media

--- src/components/profile/src/components/styled/index.js
+++ src/components/profile/src/components/styled/index.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import { StyleUtils } from 'lattice-ui-kit';
 
 import Answer from './Answer';
+
+const { media } = StyleUtils;
 
 const Body = styled.div`
   display: grid;
   grid-gap: 36px;
   grid-auto-flow: row;
+  padding: 0 30px;
+  ${media.phone`
+    padding: 0;
+  `}
 `;
 
 const SpinnerWrapper = styled.div`
