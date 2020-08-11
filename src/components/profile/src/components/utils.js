8b031b6e3388dbca5a27b0ca44e61c2336fc58e6
Solomon Tang <solomoncztang@gmail.com>
2020-08-11 16:43:58 -0700

    update formatSurveyData to use answersByQuestion map

--- src/components/profile/src/components/utils.js
+++ src/components/profile/src/components/utils.js
@@ -5,11 +5,14 @@ import {
   fromJS,
   getIn,
 } from 'immutable';
+import { Constants } from 'lattice';
 import type { Match } from 'react-router';
 
 import { PropertyTypes } from '../../../../core/edm/constants';
 import { getPropertyValue } from '../../../../utils/EntityUtils';
 import { CATEGORY_BY_QUESTION_NUMBER } from '../sagas/constants';
+
+const { OPENLATTICE_ID_FQN } = Constants;
 
 const getFirstLastFromPerson = (person :Map | Object) => {
   const firstName = getIn(person, [PropertyTypes.GIVEN_NAME, 0], '').trim();
@@ -18,13 +21,14 @@ const getFirstLastFromPerson = (person :Map | Object) => {
   return `${firstName} ${last}`;
 };
 
-const formatSurveyData = (questions :Map, answers :Map) :OrderedMap => {
+const formatSurveyData = (questions :Map, answers :Map, answersByQuestion :Map) :OrderedMap => {
   const surveyData = fromJS(CATEGORY_BY_QUESTION_NUMBER)
     .sortBy((category, number) => parseInt(number, 10))
     .reduce((grouped, category, number) => {
-      const entry = questions.findEntry((q) => getPropertyValue(q, PropertyTypes.CODE) === number);
-      if (entry) {
-        const [answerId, question] = entry;
+      const question = questions.find((q) => getPropertyValue(q, PropertyTypes.CODE) === number);
+      if (question) {
+        const questionId = getPropertyValue(question, OPENLATTICE_ID_FQN);
+        const answerId = answersByQuestion.get(questionId);
         const answer = answers.get(answerId);
         if (grouped.has(category)) {
           return grouped.mergeIn([category], fromJS([{ answer, question }]));
