6380a6375e539782c5fefd68befdb04b52d0395b
Solomon Tang <solomoncztang@gmail.com>
2020-10-08 12:37:20 -0700

    remove comment

--- src/containers/profile/src/components/HelplineProvider.js
+++ src/containers/profile/src/components/HelplineProvider.js
@@ -14,19 +14,6 @@ import {
 } from '../../../../core/redux';
 import { routerHistory } from '../../../../core/router';
 
-// import initializeReduxStore from '../../../../core/redux/ReduxStore';
-// import initializeRouterHistory from '../../../../core/router/RouterHistory';
-
-// const routerHistory = initializeRouterHistory();
-// const helplineStore = initializeReduxStore(routerHistory);
-
-// const helplineContext = React.createContext(null);
-
-// // Export your custom hooks if you wish to use them in other files.
-// export const useStore = createStoreHook(helplineContext);
-// export const useDispatch = createDispatchHook(helplineContext);
-// export const useSelector = createSelectorHook(helplineContext);
-
 export default function HelplineProvider({ children }) {
   return (
     <Provider context={helplineContext} store={helplineStore}>
